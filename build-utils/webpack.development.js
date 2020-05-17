37142fbc06172b44e0134bbda20b30be902d3097
ahmadnoor <ahmad.noor@mobilelive.ca>
2020-05-17 07:09:46 +0500

    update development  webpack config

---
+++ build-utils/webpack.development.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const path = require('path');
+const PATHS = {
+  src: path.join(__dirname, '../src/index.js'),
+  dist: path.join(__dirname, '../dist'),
+};
+module.exports = () => ({
+  entry: [
+    'react-hot-loader/patch',
+    // activate HMR for React
+    'webpack-dev-server/client?http://localhost:3000',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+    PATHS.src,
+    // the entry point of our app
+  ],
+  devtool: 'cheap-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+  ],
+  devServer: {
+    compress: true,
+    host: 'localhost',
+    port: 3000,
+    contentBase: PATHS.dist,
+    publicPath: 'http://localhost:3000/',
+    historyApiFallback: true,
+    // respond to 404s with index.html
+    hot: true,
+    watchContentBase: true,
+    stats: {
+      colors: true, chunks: true,
+    },
+    // enable HMR on the server
+    // open the browser when start server
+    open: true,
+    inline: true,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
+});
